7aa32f4a2d84a1c36f6c12fd3c2176a1d511b4ef
orpazc <orpaz129@gmail.com>
2019-04-01 14:12:26 +0300

    create post method for graph location

--- GraphCentrality/graph_api.py
+++ GraphCentrality/graph_api.py
@@ -3,11 +3,16 @@ from flask import Flask, request
 app = Flask(__name__)
 
 
-@app.route('/graph-location/<file_location>', methods=['POST'])
-def example(file_location):
+@app.route('/graph-location', methods=['POST'])
+def graph_location():
+    if request.method == 'POST':
+        nodes_path = request.json.get("nodes_path")
+        edges_path = request.json.get("edges_path")
     # call to graph loader (location)
     # call to centrality calculator
-    return "Start create new graph......."
+        return "Start create new graph......."
+    else:
+        raise Exception("Method type not allowed! use POST.")
 
 
 if __name__ == '__main__':
