19c752c5da043d546d7b3a8da9de1c8b3dc7742f
orpazc <orpaz129@gmail.com>
2019-04-01 22:12:58 +0300

    renames

--- GraphCentrality/graph_api.py
+++ GraphCentrality/graph_api.py
@@ -1,14 +1,16 @@
 from flask import Flask, request, jsonify
-from graph_utils import create_graph, get_graph_top_centers, sample_graph_details
 from datetime import datetime
+from GraphCentrality.graph_utils import create_graph, get_graph_top_centers, sample_graph_details
 
 app = Flask(__name__)
 graphs = {}
+
+
 def get_graph_obj(graph_name):
     if graph_name in graphs:
         return graphs[graph_name]
     else:
-        raise Exception(f'graph name {graph_name} not found. call /graph/<graph_name>/load_grap first')
+        raise Exception(f'graph name {graph_name} not found. call /graph/<graph_name>/load_graph first')
 
 
 @app.route('/graph/<graph_name>/load_graph', methods=['POST'])
@@ -22,11 +24,13 @@ def graph_location(graph_name):
     else:
         raise Exception("Method type not allowed! use POST.")
 
+
 @app.route('/graph/<graph_name>/get_center_nodes/<n>', methods=['GET'])
 def get_top_center_nodes(graph_name, n):
     graph_obj = get_graph_obj(graph_name)
     top_n_nodes = get_graph_top_centers(graph_obj['graph'], int(n))
     return jsonify(top_n_nodes)
+
 
 @app.route('/graph/<graph_name>/sample_details', methods=['GET'])
 def sample_node_details(graph_name):
