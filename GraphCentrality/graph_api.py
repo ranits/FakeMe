9fea83b19368b01ea4dcec79474546dcc415a848
orpazc <orpaz129@gmail.com>
2019-04-01 16:25:37 +0300

    parse result to post request

--- GraphCentrality/graph_api.py
+++ GraphCentrality/graph_api.py
@@ -1,5 +1,6 @@
 from flask import Flask, request
 from graph_utils import create_graph, calculate_graph_centrality
+import json
 
 app = Flask(__name__)
 
@@ -11,7 +12,7 @@ def graph_location():
         edges_path = request.json.get("edges_path")
         G = create_graph(nodes_file=nodes_path, edges_file=edges_path)
         top_n = calculate_graph_centrality(G)
-        return top_n
+        return json.dumps(top_n)
     else:
         raise Exception("Method type not allowed! use POST.")
 
