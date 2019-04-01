de61fcd2c54122cfbb037f0aa2743163508df1f5
Your Name <you@example.com>
2019-04-01 20:26:43 +0300

    add sample graph for average details api

--- GraphCentrality/graph_utils.py
+++ GraphCentrality/graph_utils.py
@@ -1,4 +1,6 @@
 import networkx as nx
+import numpy as np
+import pandas as pd
 
 node_details_names = ['url', 'name']
 
@@ -16,8 +18,28 @@ def create_graph(nodes_file, edges_file):
     return G
 
 
-def calculate_graph_centrality(G):
+def get_graph_top_centers(G, n):
     centrality_dict = nx.degree_centrality(G)
-    top_nodes = sorted(centrality_dict.items(), key=lambda x: x[1], reverse=True)[:5]
+    top_nodes = sorted(centrality_dict.items(), key=lambda x: x[1], reverse=True)[:n]
     top_nodes = [int(n[0]) for n in top_nodes]
     return top_nodes
+
+def sample_graph_details(G):
+    df = pd.read_csv('data/linkedin_data.csv',
+                     usecols=['m_urn', 'age', 'gender', 'ethnicity'], encoding='ISO-8859-1')
+    df = df.drop_duplicates()
+    gender_hist = df.gender.value_counts().sort_index()
+    gender_hist_probs = gender_hist / len(df.age)
+    gender_hist_values = gender_hist.index
+    gender = np.random.choice(gender_hist_values, 1, p=gender_hist_probs)[0]
+
+    ethnicity_hist = df.ethnicity.value_counts().sort_index()
+    ethnicity_hist_probs = ethnicity_hist / len(df.age)
+    ethnicity_hist_values = ethnicity_hist.index
+    ethnicity = np.random.choice(ethnicity_hist_values, 1, p=ethnicity_hist_probs)[0]
+
+    age_stats = df.groupby(['ethnicity', 'gender']).agg(['mean', 'std'])
+    relevant_stats = age_stats.loc[ethnicity, gender]
+    age = relevant_stats[1] * np.random.randn() + relevant_stats[0]
+
+    return {'gender': gender, 'ethnicity': ethnicity, 'age': age}
